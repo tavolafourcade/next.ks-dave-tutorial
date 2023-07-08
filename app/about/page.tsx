2e0dbe16b10762cd758be24d55cf5126f5999514
tavolafourcade <tavolafourcade@gmail.com>
2023-07-08 13:56:28 -0500

    [ ADD ] Links to the About page and the main Page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 const About = () => {
   return (
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <Link href='/'>Link to Home Page</Link>
+    </>
   )
 }
 export default About
